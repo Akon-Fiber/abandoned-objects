ee208aa2c8ca171a82d5d59f93141d0d459b9207
kellydavid <kellydavid178@gmail.com>
2015-12-08 00:53:23 +0000

    Initial Commit

---
+++ abandoned-objects/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  abandoned-objects
+//
+//  Created by David Kelly on 08/12/2015.
+//  Copyright © 2015 David Kelly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
